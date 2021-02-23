3dc6d9e923534a2ee43e51bf17c412601cb22e5d
olendeer <olendeer666@gmail.com>
2021-02-23 23:31:53 +0300

    Fix api

--- src/utilites/api.js
+++ src/utilites/api.js
@@ -6,8 +6,7 @@ export async function auth ( login, password ) {
         url : 'https://api.cloud.bodyhealthanalyzer.com/query',
         data: `{"operationName":null,"query":"mutation login($input:Login!) { login(input:$input){id user{id userType firstName lastName namePrefix nameSuffix phone email username roles address status language measuringSystem lastAccessed lastResetPassword isPasswordExpired createdAt caregiver {id businessName shareCode }client{id caregiverRegistrationNumber gender dateOfBirth heathNotes difficulty guide{id caregiver{id businessName shareCode }name inhaleTime holdTime exhaleTime pauseTime isPredefined createdAt }preferredBreathRate audioGuide audioType audioFile disclaimerAccepted lastTested }enabledProcedures{id procedureId name isEnabled createdAt }}expiredAt }}","variables":{"input":{"username":"${login}","email":null,"password":"${password}"}}}`,
         headers: {
-            'Content-Type' : 'application/json',
-            "Access-Control-Allow-Origin": "*"
+            'Content-Type' : 'application/json'
         }
     })
     .then(res => res.data)
@@ -20,8 +19,7 @@ export async function logout ( token ) {
         data: `{"operationName":null,"query":"mutation logout{logout}","variables":{}}`,
         headers: {
             'Content-Type' : 'application/json',
-            'Authorization' :  `Bearer ${token}`,
-            "Access-Control-Allow-Origin": "*"
+            'Authorization' :  `Bearer ${token}`
         }
     })
     .then(res => res.data)
@@ -33,8 +31,7 @@ export async function reset ( email ) {
         url : 'https://api.cloud.bodyhealthanalyzer.com/query',
         data: `{"operationName":null,"query":"mutation requestResetPassword($input:RequestResetPassword!) { requestResetPassword(input:$input)}","variables":{"input":{"email":"${email}","username":null}}}`,
         headers: {
-            'Content-Type' : 'application/json',
-            "Access-Control-Allow-Origin": "*"
+            'Content-Type' : 'application/json'
         }
     })
     .then(res => res.data)
@@ -46,8 +43,7 @@ export async function register ( data ) {
         url : 'https://api.cloud.bodyhealthanalyzer.com/query',
         data: `{"operationName":null,"query":"mutation register($input:NewUser!) { register(input:$input){id userType firstName lastName namePrefix nameSuffix phone email username roles address status language measuringSystem lastAccessed lastResetPassword isPasswordExpired createdAt caregiver {id businessName shareCode }client{id caregiverRegistrationNumber gender dateOfBirth heathNotes difficulty guide{id caregiver{id businessName shareCode }name inhaleTime holdTime exhaleTime pauseTime isPredefined createdAt }preferredBreathRate audioGuide audioType audioFile disclaimerAccepted lastTested }enabledProcedures{id procedureId name isEnabled createdAt }}}","variables":{"input":{"account":{"userType":"Caregiver","firstName":"${data.first_name}","lastName":"${data.last_name}","namePrefix":"${data.eg1}","nameSuffix":"${data.eg2}","phone":"${data.phone}","email":"${data.email}","username":"${data.username}","password":"${data.password}","address":"${data.address}","language":"enUS","measuringSystem":"Standard"},"client":null,"caregiver":{"businessName":"${data.business}"}}}}`,
         headers: {
-            'Content-Type' : 'application/json',
-            "Access-Control-Allow-Origin": "*"
+            'Content-Type' : 'application/json'
         }
     })
     .then(res => res.data)
