0e813bb4453d573b00427fbbf8e01604d45580bc
olendeer <olendeer666@gmail.com>
2021-04-13 11:32:08 +0300

    Bagfix package

---
+++ src/views/History/History.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="history-client-wrapper">
+        <div class="history-client">
+            <h2 class="history-client-header"><Back @click="$router.back()"/> History </h2>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
